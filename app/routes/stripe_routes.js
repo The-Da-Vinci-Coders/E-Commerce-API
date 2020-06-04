88b46d1c6a9bb0b2f7f26e4014ededeff6af8b08
iamcoryv <clvertentes@gmail.com>
2020-06-04 09:42:27 -0400

    Make stripe routes
    Made routes for charge, card, customer, and link card to customer.

--- app/routes/stripe_routes.js
+++ app/routes/stripe_routes.js
@@ -11,7 +11,44 @@ router.post('/stripe/charge', (req, res, next) => {
     currency: 'usd',
     customer: charge.customer
   })
-    .then(() => res.sendStatus(200))
+    .then(card => res.status(201).json({ charge: charge }))
+    .catch(next)
+})
+
+router.post('/stripe/customer', (req, res, next) => {
+  const customer = req.body.customer
+  const stripe = require('stripe')('sk_test_51Gq3g3HMAAEaJ64PeEPXPhDFJ1uHfN4czO4lBuAfkmh5a7Fr3AD6yU5ZjlKkCZeBmEgS2DsU7ks5q7p4VKassLMB00B9ej0EKe')
+  stripe.customers.create({
+    email: customer.email
+  })
+    .then(card => res.status(201).json({ customer: customer }))
+    .catch(next)
+})
+
+router.post('/stripe/card-token', (req, res, next) => {
+  const stripe = require('stripe')('sk_test_51Gq3g3HMAAEaJ64PeEPXPhDFJ1uHfN4czO4lBuAfkmh5a7Fr3AD6yU5ZjlKkCZeBmEgS2DsU7ks5q7p4VKassLMB00B9ej0EKe')
+  const card = req.body.card
+  stripe.tokens.create({
+    card: {
+      number: card.number,
+      exp_month: card.month,
+      exp_year: card.year,
+      cvc: card.cvc
+    }
+  })
+    .then(card => res.status(201).json({ card: card }))
+    .catch(next)
+})
+
+router.patch('/stripe/customer/:id', (req, res, next) => {
+  const id = req.params.id
+  const customer = req.body.customer
+  const stripe = require('stripe')('sk_test_51Gq3g3HMAAEaJ64PeEPXPhDFJ1uHfN4czO4lBuAfkmh5a7Fr3AD6yU5ZjlKkCZeBmEgS2DsU7ks5q7p4VKassLMB00B9ej0EKe')
+  stripe.customers.update(
+    `${id}`,
+    {source: customer.source}
+  )
+    .then(() => res.sendStatus(201).json({ customer: customer }))
     .catch(next)
 })
 
